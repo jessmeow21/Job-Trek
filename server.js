bef081214acbff334d709707f02efe814145bf7e
ryanward4556 <50245334+ryanward4556@users.noreply.github.com>
2019-09-09 21:05:51 -0400

    Auth (#18)
    * got basic authRoutes set up, need to check functionality, waiting for db
    
    * npm start not working, switching back to master to check its functionality
    
    * working on auth, getting user data back from google but need to store in database
    
    * added EVERYTHING

--- server.js
+++ server.js
@@ -1,40 +1,26 @@
-require("dotenv").config();
+// require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
-
-var db = require("./models");
-
-var app = express();
+var session = require("express-session");
+var passport = require("./config/passport");
 var PORT = process.env.PORT || 3000;
+var db = require("./models");
+var app = express();
 
-// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
-app.set("view engine", "handlebars");
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
