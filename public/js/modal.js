21f3c049f26bf9ef59e1f1c0405b07a2878476e5
Jessica Sulaiman <jessica.sulaiman86@gmail.com>
2019-09-11 18:12:37 -0400

    fixed lint error

--- public/js/modal.js
+++ public/js/modal.js
@@ -104,7 +104,7 @@ var modal = document.getElementById("id01");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,37 +3,35 @@ var authenticate = require("../config/authenticate");
 var path = require("path");
 
 module.exports = function(app) {
-    // Load index page
+  // Load index page
 
-    app.get("/", function(req, res) {
-        console.log(req.user);
-        if (req.user) {
-            res.redirect("/profile");
-        } else {
-            res.sendFile(path.join(__dirname, "../public/signup.html"));
-        }
-    });
+  app.get("/", function(req, res) {
+    console.log(req.user);
+    if (req.user) {
+      res.redirect("/profile");
+    } else {
+      res.sendFile(path.join(__dirname, "../public/signup.html"));
+    }
+  });
 
-    app.get("/login", function(req, res) {
-        if (req.user) {
-            res.redirect("/profile");
-        } else {
-            res.sendFile(path.join(__dirname, "../public/login.html"));
-        }
-    });
+  app.get("/login", function(req, res) {
+    if (req.user) {
+      res.redirect("/profile");
+    } else {
+      res.sendFile(path.join(__dirname, "../public/login.html"));
+    }
+  });
 
-    app.get("/landing", function(req, res) {
+  app.get("/landing", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/landing.html"));
+  });
 
-        res.sendFile(path.join(__dirname, "../public/landing.html"));
+  app.get("/profile", authenticate, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/profile.html"));
+  });
 
-    });
-
-    app.get("/profile", authenticate, function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/profile.html"));
-    });
-
-    // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
-        res.send("404");
-    });
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.send("404");
+  });
 };
