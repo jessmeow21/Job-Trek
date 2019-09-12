6d82967e6196752e4055328c070fd3925278c846
Ryan Ward <ryanward4556@gmail.com>
2019-09-12 19:10:47 -0400

    deleted comments and code from routing example that had double login and signup routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,39 +2,11 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = function(app) {
-  // Get all examples
-
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
 
   app.post("/api/signup", function(req, res) {
-    console.log("in signup route");
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
-    })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function(err) {
-        res.status(401).json(err);
-      });
-  });
-
-  // Create a new Job...
-  app.post("/api/job", function(req, res) {
-    db.Job.create(req.body).then(function(dbJob) {
-      res.json(dbJob);
-    });
-  });
-
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
-  });
-
-  app.post("/api/signup", function(req, res) {
-    console.log("in signup route");
     db.User.create({
       email: req.body.email,
       password: req.body.password
